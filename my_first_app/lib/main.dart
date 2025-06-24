4c715d61a64a9f3f8bf645c3c20e1daec69eff7c
nesrinedroul <ilhemnesrinedrl@gmail.com>
2025-06-24 14:03:08 +0100

    testing

---
+++ my_first_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:my_first_app/home_app.dart';
+
+void main() {
+  runApp(const App());
+}
+class App extends StatelessWidget {
+  const App({super.key}); // the constructure 
+ // the title of the app
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'title',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      debugShowCheckedModeBanner: false,
+      home:  
+      HomeApp('escalope'),
+      );
+  }
+}
