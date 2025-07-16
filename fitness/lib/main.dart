e8b72a5583b3090842d275614d19011e07007a5f
nesrinedroul <ilhemnesrinedrl@gmail.com>
2025-07-16 09:42:08 +0100

    updates

--- fitness/lib/main.dart
+++ fitness/lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const App());
 }
+void x(int index) {
+  // Handle navigation
+}
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -21,7 +24,22 @@ class App extends StatelessWidget {
         ),
         body: Center(child: const Text('i love samia!')),
         backgroundColor: const Color.fromARGB(255, 255, 119, 176),
-      ),
-    );
+          bottomNavigationBar: BottomNavigationBar(
+            onTap: x,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: 'Settings',
+              )],
+              backgroundColor: const Color.fromARGB(255, 255, 193, 255),
+            selectedFontSize: 16.0,
+            selectedItemColor: const Color.fromARGB(255, 66, 0, 57),
+          ),
+        ),
+      );
   }
 }
